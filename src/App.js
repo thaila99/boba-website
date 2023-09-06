a6d0a97dec435bd2d115fff261b38cd5e023908b
Thai La <thaivinhla99@gmail.com>
2023-09-06 11:20:59 -0500

    Index and navbar

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import Navbar from './Components/Navbar';
+
+function App() {
+  return (
+    <div className='App'>
+      
+      <Navbar />
+     
+
+      
+      
+    </div>
+  )
+}
+
+export default App
+
---
+++ src/Components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+     <a href="#!">Y<span>UMMY</span> CHA</a>
+     <div className='navbar-links'></div>
+    </div>
+  )
+}
+
+export default Navbar
